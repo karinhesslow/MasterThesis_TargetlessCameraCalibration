24883f9ce529bc076f698240ef840747b436dab8
karinhesslow <36153992+karinhesslow@users.noreply.github.com>
2018-03-01 13:36:20 +0100

    Add files via upload

---
+++ rotmat.m
@@ -0,0 +1,21 @@
+function R = rotmat(p, a)
+
+switch a
+    case 'x'
+        R = [1, 0, 0;
+              0, cos(p), -sin(p);
+              0, sin(p), cos(p)];
+    case 'y'
+        R = [cos(p), 0, sin(p);
+              0, 1, 0;
+              -sin(p), 0, cos(p)];
+    case 'z'
+        R = [cos(p), -sin(p), 0;
+              sin(p), cos(p), 0;
+              0, 0, 1]; 
+    otherwise
+        R = eye(3);
+end
+
+end
+
